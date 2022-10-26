e44cf926e06fc48e7499964647854f0be3973c81
unknownrocks619 <unknownrocks619@hotmail.com>
2022-10-26 20:11:24 +0100

    wp runcloud user record

--- app/Http/Controllers/Admin/User/UserController.php
+++ app/Http/Controllers/Admin/User/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
 
     public function wp_index()
     {
-        $users = WpUser::paginate(5);
+        $users = WpUser::paginate(75);
         return view('admin.users.wp-index', compact('users'));
     }
 
