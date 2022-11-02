c78c4217fac20d9b6ee120b46f4a37584bdc0cda
unknownrocks619 <unknownrocks619@hotmail.com>
2022-11-02 11:34:33 +0000

    third party chat implementation

--- resources/views/themes/frontend/master.blade.php
+++ resources/views/themes/frontend/master.blade.php
@@ -135,6 +135,7 @@
     @stack('custom_scripts')
 </body>
 <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
     var Tawk_API = Tawk_API || {},
         Tawk_LoadStart = new Date();
@@ -142,12 +143,13 @@
         var s1 = document.createElement("script"),
             s0 = document.getElementsByTagName("script")[0];
         s1.async = true;
-        s1.src = 'https://embed.tawk.to/62653dbeb0d10b6f3e6f1969/1g1dnicc2';
+        s1.src = 'https://embed.tawk.to/62653dbeb0d10b6f3e6f1969/1g1do4j59';
         s1.charset = 'UTF-8';
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
     })();
 </script>
+<!--End of Tawk.to Script-->
 <!--End of Tawk.to Script-->
 
 </html>
