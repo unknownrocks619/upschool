d2b6a3e806e7a5d72a0b7668a9205224d41d9d75
unknownrocks619 <unknownrocks619@hotmail.com>
2022-11-26 17:49:59 +0000

    session path updated

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
@@ -142,7 +142,7 @@ return [
     |
     */
 
-    'path' => '/',
+    'path' => (env('APP_ENV') == 'local' || env('APP_ENV') == 'development') ? 'dev_path' : '/',
 
     /*
     |--------------------------------------------------------------------------
@@ -181,7 +181,7 @@ return [
     |
     */
 
-    'http_only' => true,
+    'http_only' => false,
 
     /*
     |--------------------------------------------------------------------------
