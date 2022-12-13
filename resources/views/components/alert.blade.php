2f2a9bd2cee27b7f3e20e1aa073483b10343c91d
unknownrocks619 <unknownrocks619@hotmail.com>
2022-12-13 13:25:54 +0000

    font-family

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,7 +2,7 @@
 <div class="alert alert-danger alert-dismissible mb-2" role="alert">
     @foreach ($errors->all() as $error)
     <div class='d-flex align-items-center' style="font-family: 'Inter' !important;">
-        <i class="icon fas fa-exclamation"></i> &nbsp;
+        <i class="icon fas fa-exclamation-circle me-2 me-1"></i> &nbsp;
         <span style="font-family: 'Inter' !important;">{{ $error }}</span>
     </div>
     @endforeach
@@ -12,7 +12,7 @@
 @if(Session::has('info'))
 <div class="alert alert-info alert-dismissible mb-2" role="alert">
     <div class='d-flex align-items-center' style="font-family: 'Inter' !important;">
-        <i class="icon fas fa-exclamation"></i> &nbsp;
+        <i class="icon fas fa-exclamation-circle me-2"></i> &nbsp;
         <span style="font-family: 'Inter' !important;"> {{ Session::get('info') }}</span>
     </div>
 </div>
@@ -22,7 +22,7 @@
 @if(Session::has('warning'))
 <div class="alert alert-warning alert-dismissible mb-2" role="alert">
     <div class='d-flex align-items-center' style="font-family: 'Inter' !important;">
-        <i class="icon fas fa-exclamation"></i> &nbsp;
+        <i class="icon fas fa-exclamation-circle me-2"></i> &nbsp;
         <span style="font-family: 'Inter' !important;"> {{ Session::get('warning') }}</span>
     </div>
 </div>
